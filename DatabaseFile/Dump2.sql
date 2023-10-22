c13b7a4c47916ccf6c723ee6540486a8b9b22c04
Reshan Gomis <reshangomis@gmail.com>
2023-10-22 10:03:53 +0530

    rh

--- DatabaseFile/Dump2.sql
+++ DatabaseFile/Dump2.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `propertymanagementsystem` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
+USE `propertymanagementsystem`;
 -- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
 --
 -- Host: localhost    Database: propertymanagementsystem
@@ -163,4 +165,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-10-22  9:41:47
+-- Dump completed on 2023-10-22 10:03:25
